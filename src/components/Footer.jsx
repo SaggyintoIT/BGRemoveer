5211e453e3e66533f53094b64a96674088976287
SaggyintoIT <Kumarsagar500@gmail.com>
2024-05-22 22:21:12 +0530

    Adding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='bg-cyan-500/55 py-5 text-center'>
-        <p className='text-white capitalize text-xl'>created with <span className='text-red-500'>Love</span> by Santosh Kumar Gupta </p>
+        <p className='text-white capitalize text-xl'>created by Sagar KUmar </p>
     </div>
   )
 }
